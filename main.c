45e8d6853c72ef6e2de14e68c27dde37816c219a
Sergiy Horef <s@unzim.com>
2022-08-07 12:44:17 +0300

    Agent generation implemented. TODO: implement evaluation of agent fitness.

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <pthread.h>
 #include <string.h>
 
 //Definitions
@@ -40,21 +41,28 @@ struct agent {
     int y_place;
     int points;
     int objects_collected;
+    double fitness;
 
-    int dna[NUM_POSSIBLE_STATES];
+    int* dna; //int array of size NUM_POSSIBLE_STATES
 };
+#define RANDOM_POSITION 0
 
 
 //Function declarations
 void initialize_states(int level, int state);
 void setup_movements_int_state(int state);
 int power(int num, int pow);
-void setup_game_properties(int * board_width, int * board_height, int * population_size, double * mutation_probability, int * num_generations, int * min_obj_num, int * max_obj_num);
+void setup_game_properties(int * board_width, int * board_height, int * population_size, double * mutation_probability, int * num_generations, int * min_object_num, int * max_object_num);
 void please_enter_prompt(char * variable_name, int * variable_value);
 void setup_board(int ** board, int board_width, int board_height, int min_num_objects, int max_num_objects);
 
 void print_board(int ** board, int board_width, int board_height);
 void print_line(int size, char symbol);
+void generate_random_dna(int* dna);
+void *generate_random_agents(void* arg);
+void print_dna(int* dna);
+void print_agents(struct agent* agents, int size);
+void *calculate_fitness(void* arg);
 
 //Global variables
 //Array of all possible states the agent can be in.
@@ -62,7 +70,13 @@ void print_line(int size, char symbol);
 //Columns go like this: UP, RIGHT, DOWN, LEFT, CURRENT. (States of these squares.)
 int movements[NUM_POSSIBLE_STATES][NUM_VARIABLES];
 
+struct agent* population;
+struct agent* new_population;
 int main() {
+    //Variables for time measurements.
+    clock_t start, end;
+    double cpu_time_used;
+
     //Welcome message and initializing all possible agent states.
     printf("Hello, and Welcome to the Object Picking Genetic Algorithm.\n"
            "It will evolve to get around a matrix with objects and pick all of them\n");
@@ -81,38 +95,127 @@ int main() {
     setup_game_properties(&board_width, &board_height, &population_size, &mutation_probability, &number_of_generations, &min_object_number, &max_object_number);
 
     //Setting up the board will relevant parameters.
-    clock_t start, end;
-    double cpu_time_used;
+    //----Measuring the time----
     start = clock();
+    //--------
 
     int* board[board_height];
     for (int i=0; i < board_height; i++)
         board[i] = (int*)malloc(board_width * sizeof(int));
     setup_board(board, board_width, board_height, min_object_number, max_object_number);
 
+    //----Measuring the time----
     end = clock();
     cpu_time_used = ((double) (end-start))/CLOCKS_PER_SEC;
+    //--------
 
-    printf("Setting up the game board took %lf seconds\n", cpu_time_used);
+    printf("\nSetting up the game board took %lf seconds\n\n", cpu_time_used);
 
     //Generating the agents and running the algorithm.
-    //print_board(board, board_width, board_height);
 
+    //----Measuring the time----
+    start = clock();
+    //--------
+
+    //Allocating enough memory for the desired amount of population.
+    population = (struct agent*) malloc(population_size*sizeof(struct agent));
+    for (int i=0; i<population_size; i++) {
+        population[i].dna = (int*)malloc(NUM_POSSIBLE_STATES*sizeof(int));
+    }
+    //Dividing the process of setting the population to different threads.
+    pthread_t pthread1, pthread2;
+    int pthread1_size = population_size/2;
+    int pthread1_start = 0;
+    int pthread1_arg[2] = {pthread1_start, pthread1_size};
+    pthread_create(&pthread1,NULL,generate_random_agents,&pthread1_arg);
+
+    int pthread2_size = population_size-pthread1_size;
+    int pthread2_start = pthread1_size;
+    int pthread2_arg[2] = {pthread2_start, pthread2_size};
+    pthread_create(&pthread2,NULL,generate_random_agents,&pthread2_arg);
+
+    //Wait for threads to finish.
+    pthread_join(pthread1,NULL);
+    pthread_join(pthread2,NULL);
+
+    //----Measuring the time----
+    end = clock();
+    cpu_time_used = ((double) (end-start))/CLOCKS_PER_SEC;
+    //--------
+
+    printf("\nSetting up the population took %lf seconds\n\n", cpu_time_used);
+
+    //print_agents(population, population_size);
+
+
+    //------Freeing the Memory------
+    //Freeing the memory from malloc usages.
     for (int i=0;i<board_height;i++) {
         free(board[i]);
     }
+    //Freeing the dna variables.
+    for (int i=0;i<population_size;i++) {
+        free(population[i].dna);
+    }
+    //Freeing the population array.
+    free(population);
     return 0;
+}
+
+void *calculate_fitness(void* arg) {
+    int* args = (int*)arg;
+    int start = args[0];
+    int size = args[1];
+
+    for (int i=start;i<start+size;i++) {
+        struct agent current_agent = population[i];
+    }
+}
+
+int run_agent(struct agent* agent, int ** board) {
+
+}
+
+int find_state(int ** board, int board_width, int board_height, int x_val, int y_val) {
+    int state = 0;
+
+}
+
+void *generate_random_agents(void* arg) {
+    int* args = (int*)arg;
+    int start = args[0];
+    int size = args[1];
+
+    if (RANDOM_POSITION == 1) {
+        //TODO: implement random place generation.
+    } else {
+        for (int i=start;i<start+size;i++) {
+            population[i].x_place = 0;
+            population[i].y_place = 0;
+            population[i].objects_collected = 0;
+            population[i].points = 0;
+            population[i].fitness = 0;
+
+            generate_random_dna(population[i].dna);
+        }
+    }
+}
+
+void generate_random_dna(int* dna) {
+    for (int i=0;i<NUM_POSSIBLE_STATES;i++) {
+        dna[i] = rand()%NUM_MOVEMENTS;
+    }
 }
 
 void setup_board(int ** board, int board_width, int board_height, int min_num_objects, int max_num_objects) {
     for (int i=0; i<board_height; i++) {
         int j=0;
-        for (; j<board_width-4; j++) {
+        for (; j<board_width; j++) {
             board[i][j] = 0;
         }
     }
 
-    int num_objects = min_num_objects + (rand()%(max_num_objects-min_num_objects));
+    int num_objects = max_num_objects==min_num_objects ? max_num_objects : min_num_objects + (rand()%(max_num_objects-min_num_objects));
     int row, col;
     for (int i=0; i<num_objects; i++) {
         row = rand()%board_height;
@@ -162,6 +265,23 @@ void setup_movements_int_state(int state) {
         movements[last_place][i] = state%10;
         state /= 10;
     }
+}
+
+void print_agents(struct agent* agents, int size) {
+    for (int i=0; i<size; i++) {
+        printf("Agent %d: ", i);
+        print_dna(agents[i].dna);
+        printf("\n");
+    }
+}
+
+void print_dna(int* dna) {
+    printf("[");
+    for (int i=0; i<NUM_POSSIBLE_STATES; i++) {
+        printf("%d",dna[i]);
+        if (i!=NUM_POSSIBLE_STATES-1) printf(",");
+    }
+    printf("]");
 }
 
 void print_board(int ** board, int board_width, int board_height) {
